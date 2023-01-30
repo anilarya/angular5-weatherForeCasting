b7e0959237f691bf1c46a3a901796446f630078e
anilarya <anilarya@users.noreply.github.com>
2023-01-30 16:13:45 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,79 +25,79 @@ export class AppComponent implements OnInit{
   		 
   }
 
-  onSelectSearchMode(_value){
-    this.searchKey = '' 
+    onSelectSearchMode(_value){
+      this.searchKey = '' 
 
-    switch(_value) { 
-       case 'city_id': { 
-          this.placeholder = '<City Id>'
-          break; 
-       } 
-       case  'zip_code': { 
-          this.placeholder = '<Zip code>'
-          break; 
-       }
-       case  'city_name': { 
-          this.placeholder = '<City Name>'
-          break; 
-       } 
-       default: { 
-          //statements; 
-          break; 
-       } 
-    } 
-  }
+      switch(_value) { 
+         case 'city_id': { 
+            this.placeholder = '<City Id>'
+            break; 
+         } 
+         case  'zip_code': { 
+            this.placeholder = '<Zip code>'
+            break; 
+         }
+         case  'city_name': { 
+            this.placeholder = '<City Name>'
+            break; 
+         } 
+         default: { 
+            //statements; 
+            break; 
+         } 
+      } 
+    }
 
-  contructQParams(){
-    let qParams = '';
-    switch(this.searchMode) { 
-       case 'city_id': { 
-          qParams = `id=${this.searchKey}`
-          break; 
-       } 
-       case  'zip_code': { 
-          qParams = `zip=${this.searchKey}`
-          break; 
-       }
-       case  'city_name': { 
-          qParams = `q=${this.searchKey}`
-          break; 
-       } 
-       case  'geo_ordinates': { 
-          qParams = `lon=${this.longitude}&lat=${this.latitude}`
-          break; 
-       } 
-       default: { 
-          //statements; 
-          break; 
-       } 
-    } 
-  	return qParams;
-  }
+    contructQParams(){
+      let qParams = '';
+      switch(this.searchMode) { 
+         case 'city_id': { 
+            qParams = `id=${this.searchKey}`
+            break; 
+         } 
+         case  'zip_code': { 
+            qParams = `zip=${this.searchKey}`
+            break; 
+         }
+         case  'city_name': { 
+            qParams = `q=${this.searchKey}`
+            break; 
+         } 
+         case  'geo_ordinates': { 
+            qParams = `lon=${this.longitude}&lat=${this.latitude}`
+            break; 
+         } 
+         default: { 
+            //statements; 
+            break; 
+         } 
+      } 
+      return qParams;
+    }
 
-  getWeatherDetails(){
-  	let qParams =   this.contructQParams(); 
-    this.errorMessage = '';
-  	let __URL = AppConst.BASE_URI + qParams  + `&appid=${AppConst.API_KEY}`; 
-  	this.http.get(__URL).subscribe( 
-           _res => { 
-           		this.weatherForcaseDetails =  _res.json();
-           		console.error(this.weatherForcaseDetails );
-        	},
-        	_msg => {this.weatherForcaseDetails = [] ; this.errorMessage = _msg._body }
-        ); 
-  }
+    getWeatherDetails(){
+      let qParams =   this.contructQParams(); 
+      this.errorMessage = '';
+      let __URL = AppConst.BASE_URI + qParams  + `&appid=${AppConst.API_KEY}`; 
+      this.http.get(__URL).subscribe( 
+             _res => { 
+                this.weatherForcaseDetails =  _res.json();
+                console.error(this.weatherForcaseDetails );
+            },
+            _msg => {this.weatherForcaseDetails = [] ; this.errorMessage = _msg._body }
+          ); 
+    }
 
-  getTemperature(_temp :number ){
-    return (_temp - 273.15).toFixed(2)
+    getTemperature(_temp :number ){
+      return (_temp - 273.15).toFixed(2)
 
-  }
+    }
 
-  getIconImageUrl(iconName :any) {
-      return (iconName ? AppConst.ICON_BASE_URL + iconName + '.png' : '');
-  };
+    getIconImageUrl(iconName :any) {
+        return (iconName ? AppConst.ICON_BASE_URL + iconName + '.png' : '');
+    };
 
-  ngOnInit() {
-  	this.getWeatherDetails();
-  }
+    ngOnInit() {
+      this.getWeatherDetails();
+    }
 }
